d96195e31e9c9a9a83b7322a827927676b51b569
Clyde Broderick <clyderick3@gmail.com>
2022-03-20 10:18:33 -0400

    Started loop for Integer selection.

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -11,6 +11,15 @@ public class Music {
     //song si
     public Integer selection(Integer startIndex, String selection){
         //Each song is ordered sequentially = [s0, s1, ...];
+        int forward = 0;
+        int backward = 0;
+        for (int i = startIndex; i < playList.length ; i ++){
+            if (!playList[i].equals(selection)){
+                forward++;
+            }
+        }
+
+
         return null;
     }
 }
