64b4b9e54ad6a7bf9881453369ed7080cf2f9e52
Clyde Broderick <clyderick3@gmail.com>
2022-03-20 10:55:51 -0400

    Added backwardCount. Tested. Completed.

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -11,15 +11,44 @@ public class Music {
     //song si
     public Integer selection(Integer startIndex, String selection){
         //Each song is ordered sequentially = [s0, s1, ...];
+        int forward = forwardCount(startIndex, selection);
+        int backward = backwardCount(startIndex, selection);
+
+        return Math.min(forward, backward);
+    }
+
+    public Integer forwardCount(Integer startIndex, String selection) {
         int forward = 0;
-        int backward = 0;
-        for (int i = startIndex; i < playList.length ; i ++){
-            if (!playList[i].equals(selection)){
+        for (int i = startIndex; i < playList.length ; i ++) {
+            if (!playList[i].equals(selection)) {
                 forward++;
+            }
+            else {
+                break;
+            }
+        }
+        return forward;
+    }
+
+    public Integer backwardCount (Integer startIndex, String selection) {
+        int backward = 0;
+        for (int i =  startIndex; i >=0 ; i--) {
+            if (!playList[i].equals(selection)) {
+                backward++;
+            }
+            else {
+                return backward;
+            }
+        }
+        for (int i = playList.length - 1; i > 0; i--) {
+            if (!playList[i].equals(selection)){
+                backward++;
+            }
+            else {
+                return backward;
             }
         }
 
-
-        return null;
+        return backward;
     }
-}
+    }
